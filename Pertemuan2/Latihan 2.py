db3279974888fbb1567c4a12d17c4ce719b79501
stefanusmarlo <smarlo1308@gmail.com>
2025-10-25 11:00:44 +0700

    Operator Aritmatika 1

---
+++ Pertemuan2/Latihan 2.py
@@ -0,0 +1,8 @@
+
+panjang = int(input("Input long : "))
+lebar = int(input("input wide : "))
+tinggi = int(input("input height : "))
+
+volume = panjang * lebar * tinggi
+
+print("Volume balok adalah", volume, "m3") 
