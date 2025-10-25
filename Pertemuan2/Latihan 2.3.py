6910b56eca3eee50601a0254b71005f0fd9b1dd9
stefanusmarlo <smarlo1308@gmail.com>
2025-10-25 11:28:43 +0700

    Operator Aritmatika 3

---
+++ Pertemuan2/Latihan 2.3.py
@@ -0,0 +1,8 @@
+
+import math 
+jari_jari = int(input("input radius: ")) 
+tinggi = int(input("input height: "))
+
+volume = math.pi * jari_jari * tinggi
+
+print("Volume Tabung adalah", volume, "cm3")
