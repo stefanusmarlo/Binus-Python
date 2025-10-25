a82049123f47db31cec0d68f670a43352036f692
stefanusmarlo <smarlo1308@gmail.com>
2025-10-25 11:34:40 +0700

    Operator Aritmatika 4

---
+++ Pertemuan2/Latihan 2.4.py
@@ -0,0 +1,7 @@
+a = float(input("Masukkan nilai a: "))
+b = float(input("Masukkan nilai b: "))
+c = float(input("Masukkan nilai c: "))
+
+diskriminan = (b**2) - (4*a*c)
+
+print(f"Nilai diskriminan adalah: {diskriminan}")
