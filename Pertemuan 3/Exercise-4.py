d93ad687c359d50090e92cfd3f90610b6c42df2e
stefanusmarlo <smarlo1308@gmail.com>
2025-11-01 11:01:48 +0700

    Algorithms 4

---
+++ Pertemuan 3/Exercise-4.py
@@ -0,0 +1,31 @@
+def is_triangle(a, b, c):
+    return a + b > c and a + c > b and b + c > a
+
+def is_right_angled(a, b, c):
+    sides = sorted([a, b, c])
+    return abs(sides[0]**2 + sides[1]**2 - sides[2]**2) < 1e-6
+
+def determine_triangle_type(a, b, c):
+    if not is_triangle(a, b, c):
+        return "Not a Triangle"
+    elif a == b == c:
+        return "Equilateral"
+    elif a == b or b == c or a == c:
+        if is_right_angled(a, b, c):
+            return "Isosceles and Right-angled"
+        return "Isosceles"
+    elif is_right_angled(a, b, c):
+        return "Right-angled"
+    else:
+        return "Scalene"
+
+try:
+    a = float(input("Enter length of side A: "))
+    b = float(input("Enter length of side B: "))
+    c = float(input("Enter length of side C: "))
+
+    triangle_type = determine_triangle_type(a, b, c)
+    print("Triangle Type:", triangle_type)
+
+except ValueError:
+    print("Please enter valid numeric values for the sides.")
