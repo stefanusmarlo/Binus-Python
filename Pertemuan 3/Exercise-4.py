253e32d95c9477598521fabc22236b673c76d5bb
stefanusmarlo <smarlo1308@gmail.com>
2025-11-04 12:07:46 +0700

    Algorithms 4
    Refactored triangle type determination logic and added input handling.

--- Pertemuan 3/Exercise-4.py
+++ Pertemuan 3/Exercise-4.py
@@ -1,31 +1,16 @@
-triangle(a, b, c):
-    return a + b > c and a + c > b and b + c > a
+import math
 
-right_angled(a, b, c):
-    sides = sorted([a, b, c])
-    return abs(sides[0]**2 + sides[1]**2 - sides[2]**2) < 1e-6
+a = int(input("Panjang sisi A = ")) 
+b = int(input("Panjang sisi B = "))
+c = int(input("Panjang sisi C = "))
 
-determine_triangle_type(a, b, c):
-    if not is_triangle(a, b, c):
-        return "Not a Triangle"
-    elif a == b == c:
-        return "Equilateral"
-    elif a == b or b == c or a == c:
-        if is_right_angled(a, b, c):
-            return "Isosceles and Right-angled"
-        return "Isosceles"
-    elif is_right_angled(a, b, c):
-        return "Right-angled"
-    else:
-        return "Scalene"
-
-try:
-    a = float(input("Enter length of side A: "))
-    b = float(input("Enter length of side B: "))
-    c = float(input("Enter length of side C: "))
-
-    triangle_type = determine_triangle_type(a, b, c)
-    print("Triangle Type:", triangle_type)
-
-except ValueError:
-    print("Please enter valid numeric values for the sides.")
+if (a == b == c):
+    print("ini merupakan segitiga sama sisi")
+elif (a + b <= c) or (a + c <= b) or (b + c <= a): 
+    print("ini bukan merupakan segitiga") 
+elif ((a == b) or (b == c) or (c == a)):
+    print("ini merupakan segitiga sama kaki") 
+elif ((a == math.sqrt(b**2 + c**2)) or (c == math.sqrt(b**2 + a**2)) or (b == math.sqrt(a**2 + c**2))):
+    print("ini merupakan segitiga siku siku") 
+else:
+    print("ini merupakan segitiga sembarang") 
