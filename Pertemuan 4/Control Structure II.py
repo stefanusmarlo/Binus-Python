7445b2735e2eb114b1be4ec4f9946afd6f009196
stefanusmarlo <smarlo1308@gmail.com>
2025-11-08 10:03:38 +0700

    Exercise Loop 2

---
+++ Pertemuan 4/Control Structure II.py
@@ -0,0 +1,10 @@
+for i in range(0,7): 
+    print("This Iteration number " + str(i)) 
+    
+for i in range(7,1,-1): 
+    print("This Iteration number " + str(i))
+    
+str_number = ["7777777", "666666", "55555", "4444", "333", "22", "1", "22", "333", "4444", "55555", "666666", "7777777"]
+
+for i in str_number:
+    print(i) 
